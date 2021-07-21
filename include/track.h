1b10beff002e8fdf04a1f283355457647674ca8c
Zhigalenko <r.zhigalenko@mail.ru>
2021-07-21 17:36:28 +0500

    Start track )

--- include/track.h
+++ include/track.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+#include <ctime>
+
+class Track{
+    std::string titleSong;
+    std::time_t creationDate; 
+    std::size_t duration;
+public:
+    Track();
+    Track(const std::string &titleSong, const std::size_t duration);
+    ~Track();
+};
--- src/track.cpp
+++ src/track.cpp
@@ -1,1 +1,10 @@
 #include "track.h"
+
+Track::Track(){
+
+}
+
+Track::Track(const std::string &titleSong, const std::size_t duration){
+    this->titleSong = titleSong;
+    this->duration = duration;
+}
